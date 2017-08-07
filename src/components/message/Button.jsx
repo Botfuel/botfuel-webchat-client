771cc5220a7cb151a36eb9324b5f48bdb8e01d2f
louisjc <louisjc@users.noreply.github.com>
2017-08-07 18:20:43 +0200

    Button style

--- src/components/message/Button.jsx
+++ src/components/message/Button.jsx
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types';
 
 const Button = styled.button`
   background-color: ${props => (props.side === 'left' ? '#f1f0f0' : '#0084f4')};
+  color: #fff;
+  border: 0;
+  margin: 5px;
+  padding: 10px;
 `;
 
 export default function ButtonMessage({ text }) {

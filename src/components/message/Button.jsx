52e19a3e7e61c151dc5f14afb9aeefa6768111e1
louisjc <louisjc@users.noreply.github.com>
2017-08-08 11:12:18 +0200

    Adapt button theme

--- src/components/message/Button.jsx
+++ src/components/message/Button.jsx
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Button = styled.button`
-  background-color: ${props => props.theme.colors.secondary};
-  color: ${props => props.theme.colors.secondaryText};
+  background-color: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.primaryText};
   border: 0;
   margin: 5px;
   padding: 10px;
+  border-radius: 7px;
 `;
 
 export default function ButtonMessage({ text }) {

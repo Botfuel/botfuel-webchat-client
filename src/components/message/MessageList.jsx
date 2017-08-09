1e0bcd9dd97b62069a9a2f20dfeb19ecf77eb95e
louisjc <louisjc@users.noreply.github.com>
2017-08-09 17:33:04 +0200

    Add block beginning

--- src/components/message/MessageList.jsx
+++ src/components/message/MessageList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Message from './Message';
+import Block from './Block';
 
 const Messages = styled.div`
   list-style: none;
@@ -14,6 +15,13 @@ const Messages = styled.div`
 
 const MessageList = ({ messages, setRef }) =>
   (<Messages innerRef={setRef}>
+    <Block
+      value={{
+        title: 'La Poste',
+        text: 'Assistant Courier. Commandes : table, text, choices.',
+        top: true,
+      }}
+    />
     {messages.map(message =>
       <Message {...message} side={message.sender === 'user' ? 'right' : 'left'} key={message.id} />,
     )}

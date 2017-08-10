aeb103d9456ca9cef4574ebaa8041790616cc47a
louisjc <louisjc@users.noreply.github.com>
2017-08-10 15:55:35 +0200

    Fix theme

--- src/theme/laposte.js
+++ src/theme/laposte.js
@@ -2,10 +2,10 @@ export default {
   colors: {
     main: '#3d3d3d',
     mainText: '#fff',
-    primary: '#ebebeb',
-    primaryLight: '#f7f7f7',
-    secondary: '#f4b600',
-    primaryText: '#000',
-    secondaryText: 'white',
+    primary: '#f4b600',
+    primaryLight: '#000',
+    secondary: '#ebebeb',
+    primaryText: '#f7f7f7',
+    secondaryText: '#000',
   },
 };

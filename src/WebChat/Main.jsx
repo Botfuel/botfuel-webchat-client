42e90a9ae41cd9cecf2d0317e9269d5bcf16a207
Maxime Khoy <maxime.khoy@ionisx.com>
2017-08-11 12:09:45 +0200

    Split WebChat component into smart/dumb

---
+++ src/WebChat/Main.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import MessageListContainer from '../components/message/MessageListContainer';
+import Top from './Top';
+import Bottom from './Bottom';
+
+const Container = styled.div`
+  transition: opacity 0.5s ease-in-out;
+  display: ${props => (props.isVisible ? 'block' : 'none')};
+  color: #000;
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+  box-shadow: 0 0 24px rgba(0, 0, 0, .15);
+  border: 1px solid #e3e3e3;
+  border-radius: 10px;
+  overflow: hidden;
+  @media (max-height: ${props => props.height + 20}px),
+    (max-width: ${props => props.width + 20}px),
+    ${props => props.fullScreen && 'all'} {
+    width: 100vw;
+    height: 100vh;
+    border-radius: 0;
+    border: 0;
+  }
+`;
+
+const Main = ({
+  messages,
+  width,
+  height,
+  isVisible,
+  fullScreen,
+  switchMode,
+  toggleFullScreen,
+  sendAction,
+  sendMessage,
+  handleKeyPress,
+  handleInputChange,
+  input,
+}) =>
+  (<Container
+    width={width || width}
+    height={height || height}
+    isVisible={isVisible}
+    fullScreen={fullScreen}
+  >
+    <Top fullScreen={fullScreen} switchMode={switchMode} switchSize={toggleFullScreen} />
+    <MessageListContainer sendAction={sendAction} messages={messages} />
+    <Bottom
+      sendMessage={sendMessage}
+      onKeyPress={handleKeyPress}
+      onInputChange={handleInputChange}
+      input={input}
+    />
+  </Container>);
+
+Main.propTypes = {
+  switchMode: PropTypes.func.isRequired,
+  toggleFullScreen: PropTypes.func.isRequired,
+  isVisible: PropTypes.bool.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  fullScreen: PropTypes.bool.isRequired,
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      user: PropTypes.string,
+      bot: PropTypes.string,
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+      sender: PropTypes.string,
+      type: PropTypes.string,
+    }),
+  ),
+  sendAction: PropTypes.func.isRequired,
+  sendMessage: PropTypes.func.isRequired,
+  handleKeyPress: PropTypes.func.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  input: PropTypes.string,
+};
+
+Main.defaultProps = {
+  messages: [],
+  input: '',
+};
+
+export default Main;
--- src/WebChat/index.jsx
+++ src/WebChat/index.jsx
@@ -1,91 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import { gql, graphql, compose } from 'react-apollo';
-import MessageListContainer from '../components/message/MessageListContainer';
-import Top from './Top';
-import Bottom from './Bottom';
+import Main from './Main';
 
-const Container = styled.div`
-  transition: opacity 0.5s ease-in-out;
-  display: ${props => (props.isVisible ? 'block' : 'none')};
-  color: #000;
-  width: ${props => props.width}px;
-  height: ${props => props.height}px;
-  box-shadow: 0 0 24px rgba(0, 0, 0, .15);
-  border: 1px solid #e3e3e3;
-  border-radius: 10px;
-  overflow: hidden;
-  @media (max-height: ${props => props.height + 20}px),
-    (max-width: ${props => props.width + 20}px),
-    ${props => props.fullScreen && 'all'} {
-    width: 100vw;
-    height: 100vh;
-    border-radius: 0;
-    border: 0;
+const MessageFragment = gql`
+  fragment FullMessage on Message {
+    id
+    type
+    user
+    bot
+    sender
+    value {
+      ... on Text {
+        text
+      }
+      ... on Table {
+        schema {
+          key
+          label
+        }
+        rows
+      }
+      ... on Choices {
+        choices {
+          id
+          text
+          payload
+        }
+      }
+    }
   }
 `;
 
 const MESSAGES_QUERY = gql`
   query messages($user: ID!, $bot: ID!) {
     messages(user: $user, bot: $bot) {
-      id
-      user
-      bot
-      type
-      value {
-        ... on Text {
-          text
-        }
-        ... on Table {
-          schema {
-            key
-            label
-          }
-          rows
-        }
-        ... on Choices {
-          choices {
-            id
-            text
-            payload
-          }
-        }
-      }
-      sender
+      ...FullMessage
     }
   }
+  ${MessageFragment}
 `;
 
 const MESSAGES_SUBSCRIPTION = gql`
   subscription onMessageAdded($user: ID!, $bot: ID!) {
     messageAdded(user: $user, bot: $bot) {
-      id
-      user
-      bot
-      value {
-        ... on Text {
-          text
-        }
-        ... on Table {
-          schema {
-            key
-            label
-          }
-          rows
-        }
-        ... on Choices {
-          choices {
-            id
-            text
-            payload
-          }
-        }
-      }
-      sender
-      type
+      ...FullMessage
     }
   }
+  ${MessageFragment}
 `;
 
 const MESSAGE_MUTATION = gql`
@@ -105,32 +67,10 @@ const MESSAGE_MUTATION = gql`
       sender: $sender
       type: $type
     ) {
-      id
-      user
-      bot
-      value {
-        ... on Text {
-          text
-        }
-        ... on Table {
-          schema {
-            key
-            label
-          }
-          rows
-        }
-        ... on Choices {
-          choices {
-            id
-            text
-            payload
-          }
-        }
-      }
-      sender
-      type
+      ...FullMessage
     }
   }
+  ${MessageFragment}
 `;
 
 class WebChat extends React.Component {
@@ -185,6 +125,7 @@ class WebChat extends React.Component {
           sender: 'user',
         },
       });
+
       // If subscriptions are not used, we need to refetch manually the message that was just
       // sent by the user so he gets immediate success feedback on his own message
       if (!window.WebSocket) {
@@ -225,36 +166,20 @@ class WebChat extends React.Component {
     const messages = this.props.messages.filter((m, index) => !isQuickRepliesAndNotLast(m, index));
 
     return (
-      <Container
-        width={this.state.width || this.props.width}
-        height={this.state.height || this.props.height}
-        isVisible={this.props.isVisible}
-        fullScreen={this.props.fullScreen}
-      >
-        <Top
-          fullScreen={this.props.fullScreen}
-          switchMode={this.props.switchMode}
-          switchSize={this.props.toggleFullScreen}
-        />
-        <MessageListContainer sendAction={this.sendAction} messages={messages || []} />
-        <Bottom
-          sendMessage={this.sendMessage}
-          onKeyPress={this.handleKeyPress}
-          onInputChange={this.handleInputChange}
-          input={this.state.input}
-        />
-      </Container>
+      <Main
+        {...this.props}
+        {...this.state}
+        messages={messages}
+        sendAction={this.sendAction}
+        sendMessage={this.sendMessage}
+        handleKeyPress={this.handleKeyPress}
+        handleInputChange={this.handleInputChange}
+      />
     );
   }
 }
 
 WebChat.propTypes = {
-  switchMode: PropTypes.func.isRequired,
-  toggleFullScreen: PropTypes.func.isRequired,
-  isVisible: PropTypes.bool.isRequired,
-  width: PropTypes.number.isRequired,
-  height: PropTypes.number.isRequired,
-  fullScreen: PropTypes.bool.isRequired,
   messages: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string,

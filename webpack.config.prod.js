a2d9fa46df202f52a545a8177b5b1b9adbe5ac52
Maxime Khoy <windkomo@gmail.com>
2017-09-18 11:39:37 +0200

    Fix images

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -47,7 +47,7 @@ module.exports = {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: 'url-loader',
             options: {},
           },
         ],

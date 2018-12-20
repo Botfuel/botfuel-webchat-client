96db0b1cd64ded07255b91bb023263d1f51f6726
Kévin Depuydt <kevin.dpdt@gmail.com>
2018-12-20 15:31:29 +0100

    Message referrer url (#36)
    * added referrer to user messages
    
    * set default value to null if window.location.href is not defined
    
    * added referrer to postback and quickreplies messages

--- src/components/WebChat/index.jsx
+++ src/components/WebChat/index.jsx
@@ -137,8 +137,8 @@ const MESSAGES_SUBSCRIPTION = gql`
 `;
 
 const TEXT_MESSAGE_MUTATION = gql`
-  mutation createTextMessage($user: ID!, $bot: ID!, $value: String!, $sender: String!, $localMessageId: String) {
-    createTextMessage(user: $user, bot: $bot, value: $value, sender: $sender, localMessageId: $localMessageId) {
+  mutation createTextMessage($user: ID!, $bot: ID!, $value: String!, $sender: String!, $referrer: String, $localMessageId: String) {
+    createTextMessage(user: $user, bot: $bot, value: $value, sender: $sender, referrer: $referrer, localMessageId: $localMessageId) {
       ...FullMessage
     }
   }
@@ -152,8 +152,9 @@ const POSTBACK_MESSAGE_MUTATION = gql`
     $value: JSON!
     $text: String!
     $sender: String!
+    $referrer: String
   ) {
-    createPostbackMessage(user: $user, bot: $bot, value: $value, text: $text, sender: $sender) {
+    createPostbackMessage(user: $user, bot: $bot, value: $value, text: $text, sender: $sender, referrer: $referrer) {
       ...FullMessage
     }
   }
@@ -249,6 +250,7 @@ class WebChat extends React.Component {
         bot: this.props.botId,
         value: text,
         sender: 'user',
+        referrer: window.location.href || null,
         localMessageId: uuidv4(),
       };
       // Add local message
@@ -274,6 +276,7 @@ class WebChat extends React.Component {
           bot: this.props.botId,
           value,
           sender: 'user',
+          referrer: window.location.href || null,
           ...(!!text && {
             text,
           }),
